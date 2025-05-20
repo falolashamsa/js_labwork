e088a831c652148c34c1a0f47244a5a8982fbe20
Shamsa <falolashamsa@gmail.com>
2025-05-20 04:51:05 +0100

    Did first lab

---
+++ firstlab/lab1.js
@@ -0,0 +1,43 @@
+// labwork1
+
+
+// 1st possibility
+
+// let x;
+// let y;
+// x = 6;
+// y = 4;
+// x += y;
+// console.log(x)
+
+// 2nd possibility
+
+let x;
+let y;
+x = 5;
+y = 5;
+x += y
+console.log(x)
+
+
+// labwork2
+
+
+// p > q and  p <= q = False
+// 1. one statement can be true while the other is wrong
+// 2. both expressions are wrong.
+
+
+// labwork3
+
+
+let a = 7;
+a = a + " ";
+console.log(typeof(a))
+// it was added to a string
+
+
+// labwork 4
+
+
+// numbers, can be used for numerical calculation and can also appear as a string
